b46e0607c64d91c51ea3c7714161099ce2b48193
ejmarinc <ejmarinc@hotmail.com>
2018-09-16 00:12:43 -0600

    getter and setter methods created

--- src/Tarea1ErickMarinCartin/Apuesta.java
+++ src/Tarea1ErickMarinCartin/Apuesta.java
@@ -8,4 +8,29 @@ public class Apuesta{
     private int montoEquipo1 = 0;
     private int montoEmpate = 0;
     private int montoEquipo2 = 0;
+//    Setter methods
+    public void setCedula(String cedula)
+    {
+        this.cedula = cedula;
+    }
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    public void setNumeroDePartido(int numeroDePartido)
+    {
+        this.numeroDePartido = numeroDePartido;
+    }
+    public void setMontoEquipo1 (int montoEquipo1)
+    {
+        this.montoEquipo1 = montoEquipo1;
+    }
+    public void setMontoEmpate (int montoEmpate)
+    {
+        this.montoEmpate = montoEmpate;
+    }
+    public void setMontoEquipo2 (int montoEquipo2)
+    {
+        this.montoEquipo2 = montoEquipo2;
+    }
 }
--- src/Tarea1ErickMarinCartin/DatosPartido.java
+++ src/Tarea1ErickMarinCartin/DatosPartido.java
@@ -3,8 +3,8 @@ package Tarea1ErickMarinCartin;
 public class DatosPartido {
 
     private int numeroDePartido; // Atributos Partido
-    private String fecha;
-    private String hora;
+    private String fechaPartido;
+    private String horaPartido;
     private int factorEmpate;
     private int codigoEquipo1; // Atributos Equipo 1
     private String nombreEquipo1;
@@ -15,4 +15,106 @@ public class DatosPartido {
     private float factorEquipo2;
     private int golesEquipo2 = 0;
 
+    
+
+//Setter Methods
+    public void setNumeroDePartido(int numeroDePartido)
+    {
+        this.numeroDePartido = numeroDePartido;
+    }
+    public void setFechaPartido(String fechaPartido)
+    {
+        this.fechaPartido = fechaPartido;
+    }
+    public void setHoraPartido(String horaPartido)
+    {
+        this.horaPartido = horaPartido;
+        }
+    public void setFactorEmpate(int factorEmpate)
+    {
+        this.factorEmpate = factorEmpate;
+    }
+    public void setCodigoEquipo1 (int codigoEquipo1)
+    {
+        this.codigoEquipo1 = codigoEquipo1;
+    }
+    public void setNombreEquipo1 (String nombreEquipo1)
+    {
+        this.nombreEquipo1 = nombreEquipo1;
+            }
+    public void setFactorEquipo1 (float factorEquipo1)
+    {
+        this.factorEquipo1 = factorEquipo1;
+    }
+    public void setGolesEquipo1 (int golesEquipo1)
+    {
+        this.golesEquipo1 = golesEquipo1;
+    }
+    public void setCodigoEquipo2 (int codigoEquipo2)
+    {
+        this.golesEquipo1 = codigoEquipo2;
+    }
+    public void setNombreEquipo2 (String nombreEquipo2)
+    {
+        this.nombreEquipo2 = nombreEquipo2;
+    }
+    public void setFactorEquipo2 (float factorEquipo2)
+    {
+        this.factorEquipo2 = factorEquipo2;
+    }
+    public void setGolesEquipo2 (int golesEquipo2)
+    {
+        this.golesEquipo2 = golesEquipo2;
+    }
+// Getter methods
+
+
+    public String getHoraPartido() {
+        return horaPartido;
+    }
+
+    public int getFactorEmpate() {
+        return factorEmpate;
+    }
+
+    public int getCodigoEquipo1() {
+        return codigoEquipo1;
+    }
+
+    public String getNombreEquipo1() {
+        return nombreEquipo1;
+    }
+
+    public float getFactorEquipo1() {
+        return factorEquipo1;
+    }
+
+    public int getGolesEquipo1() {
+        return golesEquipo1;
+    }
+
+    public int getCodigoEquipo2() {
+        return codigoEquipo2;
+    }
+
+    public String getNombreEquipo2() {
+        return nombreEquipo2;
+    }
+
+    public float getFactorEquipo2() {
+        return factorEquipo2;
+    }
+
+    public int getGolesEquipo2() {
+        return golesEquipo2;
+    }
+
+    public int getNumeroDePartido() {
+        return numeroDePartido;
+    }
+
+//    Create setter methods
+
+
 }
+
